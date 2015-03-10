4910c3aeba86a93ca057d0cdeb097adb319c8f99
Alex Nichol <unixpickle@gmail.com>
2015-03-10 11:50:51 -0400

    added Proxy

--- proxy.go
+++ proxy.go
@@ -1,6 +1,10 @@
 package reverseproxy
 
-import "net/http"
+import (
+	"math/rand"
+	"net/http"
+	"sync"
+)
 
 // ProxyRequest proxies a request to a given host.
 // This will handle WebSockets intelligently.
@@ -19,4 +23,64 @@ func proxyRequest(w http.ResponseWriter, r *http.Request, hosts []string,
 	} else {
 		proxyHTTP(w, r, hosts, indices)
 	}
+}
+
+// A RuleTable associates zero or more target hosts with a destination hosts.
+type RuleTable map[string][]string
+
+// Copy returns a deep copy of a RuleTable.
+func (r RuleTable) Copy() RuleTable {
+	res := RuleTable{}
+	for key, val := range r {
+		res[key] = make([]string, len(val))
+		copy(res[key], val)
+	}
+	return res
+}
+
+// A Proxy handles HTTP requests and forwards them through a rule table.
+type Proxy struct {
+	lock  sync.RWMutex
+	rules RuleTable
+}
+
+// NewProxy creates a Proxy with an initial RuleTable.
+func NewProxy(rules RuleTable) *Proxy {
+	return &Proxy{sync.RWMutex{}, rules.Copy()}
+}
+
+// RuleTable returns the Proxy's current rule table.
+func (p *Proxy) RuleTable() RuleTable {
+	p.lock.RLock()
+	defer p.lock.RUnlock()
+	return p.rules.Copy()
+}
+
+// ServeHTTP routes a request to the HTTP server.
+func (p *Proxy) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	// Pick a host
+	p.lock.RLock()
+	rules, found := p.rules[r.URL.Host]
+	if !found {
+		// This is the "no forward rule" rule
+		rules, found = p.rules["*"]
+	}
+	if !found {
+		p.lock.RUnlock()
+		w.Write([]byte("No forward rule found."))
+		return
+	}
+	hosts := make([]string, len(rules))
+	copy(hosts, rules)
+	p.lock.RUnlock()
+
+	// For now, load balancing will be random.
+	proxyRequest(w, r, hosts, rand.Perm(len(hosts)))
+}
+
+// SetRuleTable updates the rule table used by the Proxy.
+func (p *Proxy) SetRuleTable(t RuleTable) {
+	p.lock.Lock()
+	p.rules = t
+	p.lock.Unlock()
 }
